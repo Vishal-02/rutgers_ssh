de8e51be7fd337ed85ccf690cea2b35e30f08970
Vishal Singh <vishal.aksingh@gmail.com>
2024-12-26 03:58:03 -0500

    trying mp

---
+++ test.py
@@ -0,0 +1,24 @@
+import multiprocessing as mp
+import os
+import time
+
+def find_prime(n):
+    if n in [1, 2, 3]:
+        return n
+
+    flag = False
+    for i in range(2, n // 2):
+        if int(n / i) == n / i:
+            flag = True
+            break
+
+    if not flag:
+        print(f"Process {os.getpid() % 10} found the prime number: {n}")
+        # q.put(n)
+        return n
+
+if __name__ == '__main__':
+    q = mp.Queue()
+    with mp.Pool(processes=10) as pool:
+        for i in pool.imap_unordered(find_prime, range(1000)):
+            pass
