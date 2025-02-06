b86d734a45bea874f7f43c43c9526219894fa2b3
Vishal-02 <vishal.aksingh@gmail.com>
2025-02-06 10:33:46 -0500

    figure out why mp is slower than normal

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import multiprocessing as mp
 import os
 import time
-# test comment
+from math import sqrt
+
 def find_prime(n):
     if n in [1, 2, 3]:
         return n
 
     flag = False
-    for i in range(2, n // 2):
+    for i in range(2, round(sqrt(n))):
         if int(n / i) == n / i:
             flag = True
             break
@@ -18,7 +19,23 @@ def find_prime(n):
         return n
 
 if __name__ == '__main__':
+    tic = time.time()
     q = mp.Queue()
-    with mp.Pool(processes=10) as pool:
-        for i in pool.imap_unordered(find_prime, range(1000)):
+    with mp.Pool(processes=32) as pool:
+        for i in pool.imap_unordered(find_prime, range(1_000_000)):
             pass
+    toc = time.time()
+    time1 = toc - tic
+
+    tic = time.time()
+    for i in range(1, 1_000_000):
+        find_prime(i)
+    toc = time.time()
+    time2 = toc - tic
+
+    print(f"It took us {time1} seconds using mp")
+    print(f"It took us {time2} seconds without mp")
+
+    print(f"that's a {(time2 / time1) * 100} % difference")
+
+    print(time2 > time1)
